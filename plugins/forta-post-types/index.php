f02edc814758bc4ff0deba5a70310c0269fa6267
Cory Pierson <cpierson@delianet.com>
2017-09-13 14:45:59 -0400

    Added to gitignore

---
+++ plugins/forta-post-types/index.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Plugin Name: Forta Post Type Plugins
+ * Plugin URI: 
+ * Description: This plugin is intended to be used with the Forta Websites to add the custom post types needed.
+ * Version: 1.0
+ * Author: John Horoszewski for Delia Associates
+ * Author URI: http://www.johnswork.com, http://www.delianet.com
+ */
+
+// Register Project Custom Post Type
+function forta_project_cpt() {
+
+	$labels = array(
+		'name'                  => _x( 'Projects', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Project', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Projects', 'text_domain' ),
+		'name_admin_bar'        => __( 'Projects', 'text_domain' ),
+		'archives'              => __( 'Project Archives', 'text_domain' ),
+		'attributes'            => __( 'Project Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Project:', 'text_domain' ),
+		'all_items'             => __( 'All Projects', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Project', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Project', 'text_domain' ),
+		'edit_item'             => __( 'Edit Project', 'text_domain' ),
+		'update_item'           => __( 'Update Project', 'text_domain' ),
+		'view_item'             => __( 'View Project', 'text_domain' ),
+		'view_items'            => __( 'View Projects', 'text_domain' ),
+		'search_items'          => __( 'Search Project', 'text_domain' ),
+		'not_found'             => __( 'Project Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Project Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Project Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set Project image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove Project image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as project image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into project', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this project', 'text_domain' ),
+		'items_list'            => __( 'Projects list', 'text_domain' ),
+		'items_list_navigation' => __( 'Projects list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter projects list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Project', 'text_domain' ),
+		'description'           => __( 'Project Custom Post Type', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'page-attributes', 'post-formats', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-category',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'Project', $args );
+
+}
+add_action( 'init', 'forta_project_cpt', 0 );
+
+
+// Register Product Custom Post Type
+function forta_product_cpt() {
+
+	$labels = array(
+		'name'                  => _x( 'Products', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Product', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Products', 'text_domain' ),
+		'name_admin_bar'        => __( 'Products', 'text_domain' ),
+		'archives'              => __( 'Product Archives', 'text_domain' ),
+		'attributes'            => __( 'Product Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Product:', 'text_domain' ),
+		'all_items'             => __( 'All Products', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Product', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Product', 'text_domain' ),
+		'edit_item'             => __( 'Edit Product', 'text_domain' ),
+		'update_item'           => __( 'Update Product', 'text_domain' ),
+		'view_item'             => __( 'View Product', 'text_domain' ),
+		'view_items'            => __( 'View Products', 'text_domain' ),
+		'search_items'          => __( 'Search Product', 'text_domain' ),
+		'not_found'             => __( 'Product Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Product Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Product Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set Product image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove Product image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as product image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into product', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this product', 'text_domain' ),
+		'items_list'            => __( 'Products list', 'text_domain' ),
+		'items_list_navigation' => __( 'Products list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter product list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Product', 'text_domain' ),
+		'description'           => __( 'Product Custom Post Type', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'page-attributes', 'post-formats', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-tag',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'Product', $args );
+
+}
+add_action( 'init', 'forta_product_cpt', 0 );
