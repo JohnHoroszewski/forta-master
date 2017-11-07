dd49eb3978f59546d2035be9c075a303beb9c1e5
Cory Pierson <cpierson@delianet.com>
2017-11-07 11:23:41 -0500

    Moved Call To Action on Subpages Above Product Tabs section

--- themes/forta-master/template-parts/content-product.php
+++ themes/forta-master/template-parts/content-product.php
@@ -62,6 +62,21 @@
 		</aside>
 	</div>
 
+	<?php if ( get_theme_mod( 'forta_master_large_text' ) ) : ?>
+	<section class="pro-cta" style="background-image: url( '<?php echo get_theme_mod( 'forta_master_products_image' ); ?>' );">
+		<div class="constrain">
+			<div class="left-block">
+				<span class="small-text site-font-accent"><?php echo get_theme_mod( 'forta_master_small_text' ); ?></span>
+				<span class="large-text top"><?php echo get_theme_mod( 'forta_master_large_top_text' ); ?></span>
+				<span class="large-text bottom"><?php echo get_theme_mod( 'forta_master_large_bottom_text' ); ?></span>
+			</div>
+				<div class="right-block">
+					<a class="cta-btn site-accent" href="<?php echo get_theme_mod( 'forta_master_button_link' ); ?>"><?php echo get_theme_mod( 'forta_master_button_text' ); ?></a>
+				</div>
+		</div>
+	</section>
+	<?php endif; ?>
+
 	<?php if ( have_rows( 'product_tabs' ) ) : ?>
 		<section class="product-details">
 			<ul class="product-tabs">
@@ -119,21 +134,6 @@
 		</section>
 
 	<?php endif; ?><!-- End Product Details -->
-
-	<?php if ( get_theme_mod( 'forta_master_large_text' ) ) : ?>
-	<section class="pro-cta" style="background-image: url( '<?php echo get_theme_mod( 'forta_master_products_image' ); ?>' );">
-		<div class="constrain">
-			<div class="left-block">
-				<span class="small-text site-font-accent"><?php echo get_theme_mod( 'forta_master_small_text' ); ?></span>
-				<span class="large-text top"><?php echo get_theme_mod( 'forta_master_large_top_text' ); ?></span>
-				<span class="large-text bottom"><?php echo get_theme_mod( 'forta_master_large_bottom_text' ); ?></span>
-			</div>
-				<div class="right-block">
-					<a class="cta-btn site-accent" href="<?php echo get_theme_mod( 'forta_master_button_link' ); ?>"><?php echo get_theme_mod( 'forta_master_button_text' ); ?></a>
-				</div>
-		</div>
-	</section>
-	<?php endif; ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
 
